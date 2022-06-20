d64f9ef323d9407d74eb1156af5bfcb8e1d18ec3
unknown <you@example.com>
2022-06-20 15:54:06 +0600

    first app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './componets/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+       <h1>ki vai Thik ache?</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return(
+        <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Link one</li>
+                <li>Link Two</li>
+                <li> About Us</li>
+                <li>Contuct Us</li>
+            </ul>
+        </nav>
+        </>
+    )
+    }
+    export default Navbar;
